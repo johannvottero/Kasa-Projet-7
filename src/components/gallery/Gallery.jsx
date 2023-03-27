7de7fbe45cacb150ee01ecd80a23437cb96732ca
Johann <johannvottero@gmail.com>
2023-03-27 18:42:59 +0200

    addition of cards on home page

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import datas from '../../data/data'
+import Card from '../cards/Cards'
 
-const Gallery = () => {
-	return (
-        <div className="gallery-Container"></div>
-	);
-};
+export default function Gallery() {
 
-export default Gallery;
+    return (
+        <main className='home_gallery'>
+            {datas.map(data => {
+                return (
+                    <Card
+                        key={data.id}
+                        id={data.id}
+                        title={data.title}
+                        cover={data.cover}
+                    />
+                )
+            })}
+        </main>
+    )
+}

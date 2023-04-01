6f0dced4feafe80aebf80b8e576914124b921890
Johann <johannvottero@gmail.com>
2023-04-01 11:53:43 +0200

    responsive version

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -6,51 +6,15 @@ import DropdownLarge from "../../components/DropdownLarge/DropdownLarge";
 	export default function AboutUs () {
 		return (
 			<div className='collapse_container main-container'>
-				<Banner src=""/>
+				<Banner/>
+				<div className="collapse_container_main">
 				<DropdownLarge/>
 				<DropdownLarge/>
 				<DropdownLarge/>
 				<DropdownLarge/>
+				</div>
 			</div>
 		)
 	}
 	
-
-/* 	return (
-		<div className="main_container">
-				<figure className="banner">
-				<img className ="banner-img" src={banner} alt="côte surplombée de forêts"></img>
-			</figure>
-
-			<div className="collapse_container">
-			<div className="collapse_container-heading">
-				<div className="collapse_container-title"> <span>Fiabilité</span></div>
-				<div className="collapse_container-chevron">
-					<img src={chevron} alt="chevron collapse"></img>
-				</div>
-			</div>
-			<div className="collapse_container-heading">
-				<div className="collapse_container-title"> <span>Respect</span></div>
-				<div className="collapse_container-chevron">
-					<img src={chevron} alt="chevron collapse"></img>
-				</div>
-			</div>
-
-			<div className="collapse_container-heading">
-				<div className="collapse_container-title"> <span>Service</span></div>
-				<div className="collapse_container-chevron">
-					<img src={chevron} alt="chevron collapse"></img>
-				</div>
-			</div>
-
-			<div className="collapse_container-heading">
-				<div className="collapse_container-title"> <span>Responsabilité</span></div>
-				<div className="collapse_container-chevron">
-					<img src={chevron} alt="chevron collapse"></img>
-				</div>
-			</div>
-
-			</div>
-		</div> 
-	); */
 
--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -5,37 +5,38 @@ import DropdownLarge from "../../components/DropdownLarge/DropdownLarge";
 import redstar from "../../assets/Vectorredstar.png";
 import greystar from "../../assets/Vectorgreystar.png";
 
-
-/* import VectorgreyStar from '../../assets/Vectorgreystar.png';
-import VectorredStar from '../../assets/Vectorredstar.png'; */
-
 const Lodging = () => {
 
 	return (
 		<div className="lodging main-container">
 			<Gallery/>
 			<Link className='lodging' to='/lodging'></Link>
-			<div className="lodging-heading">
-				<div className="lodging-heading-main">
-					<div className="lodging-heading-main-location" ></div>
-					<div className="lodging-heading-main__title"><h1>Magnifique appartement proche Canal Saint Martin</h1></div>
-					<div className="lodging-heading-main__location">Ile de France - Paris 20e</div>
+			<div className="lodging-container">
+				<div className="lodging-heading">
+					<div className="lodging-heading-main">
+						<div className="lodging-heading-main-location" ></div>
+						<div className="lodging-heading-main__title"><h1>Magnifique appartement proche Canal Saint Martin</h1></div>
+						<div className="lodging-heading-main__location">Ile de France - Paris 20e</div>
+						<div className="lodging-subheading-tags">
+							<div className="lodging-subheading-tags-pill">Cozy</div>
+						</div>
+					</div>
+				
 				</div>
-				<div className="lodging-heading-main-owner">
-					<div className="lodging-heading-main-owner__name">Franck Maher</div>
-					<div className="lodging-heading-main-owner__picture"> <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-2.jpg" alt="" /></div>
+				<div className="lodging-subheading">
+					<div className="lodging-heading-main-owner">
+						<div className="lodging-heading-main-owner__name">Franck Maher</div>
+						<div className="lodging-heading-main-owner__picture"> <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-2.jpg" alt="" /></div>
+					</div>
+
+					<div className="lodging-subheading__ratings">
+					<img src={redstar} alt=""/>
+					<img src={redstar} alt=""/>
+					<img src={redstar} alt=""/>
+					<img src={greystar} alt=""/>
+					<img src={greystar} alt=""/>
+					</div>
 				</div>
-			</div>
-			<div className="lodging-subheading">
-				<div className="lodging-subheading-tags">
-					<div className="lodging-subheading-tags-pill">Cozy</div>
-				</div>
-				<div className="lodging-subheading__ratings">
-				 <img src={redstar} alt=""/>
-				 <img src={redstar} alt=""/>
-				 <img src={redstar} alt=""/>
-				 <img src={greystar} alt=""/>
-				 </div>
 			</div>
 			<div className="lodging-collapse-container"> 
 			<div className="lodging-collapse-container__rightColumn">
